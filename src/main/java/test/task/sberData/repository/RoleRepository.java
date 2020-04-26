c12abb5ec30a4656917afdaad68df51a0fc193bb
nick <gnn@mm.st>
2020-04-26 19:31:34 +0300

    add roles repo

--- src/main/java/test/task/sberData/repository/RoleRepository.java
+++ src/main/java/test/task/sberData/repository/RoleRepository.java
@@ -1,9 +1,9 @@
-package test.task.sberData.dao;
+package test.task.sberData.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 import test.task.sberData.entities.Role;
 
 @Component
-public interface RoleRepository extends JpaRepository<Long, Role> {
+public interface RoleRepository extends JpaRepository<Role, Long> {
 }
