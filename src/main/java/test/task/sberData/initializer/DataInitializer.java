276cd24465ea7262790a7388b648fd4c6d0bfc80
nick <gnn@mm.st>
2020-04-26 19:29:47 +0300

    add roles by start

--- src/main/java/test/task/sberData/initializer/DataInitializer.java
+++ src/main/java/test/task/sberData/initializer/DataInitializer.java
@@ -1,4 +1,17 @@
 package test.task.sberData.initializer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import test.task.sberData.entities.Role;
+import test.task.sberData.service.RoleService;
+
+@Component
 public class DataInitializer {
+    @Autowired
+    private RoleService roleService;
+
+    public void init() {
+        roleService.addRole(new Role("ROLE_ADMIN"));
+        roleService.addRole(new Role("ROLE_USER"));
+    }
 }
