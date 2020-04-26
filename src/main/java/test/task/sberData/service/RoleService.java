2412660928e99edf201e77fba9f7b044ad0f8f46
nick <gnn@mm.st>
2020-04-26 19:30:36 +0300

    add roles services

--- src/main/java/test/task/sberData/service/RoleService.java
+++ src/main/java/test/task/sberData/service/RoleService.java
@@ -1,4 +1,7 @@
 package test.task.sberData.service;
 
+import test.task.sberData.entities.Role;
+
 public interface RoleService {
+    void addRole(Role role);
 }
--- src/main/java/test/task/sberData/service/impl/RoleServiceImpl.java
+++ src/main/java/test/task/sberData/service/impl/RoleServiceImpl.java
@@ -1,4 +1,25 @@
 package test.task.sberData.service.impl;
 
-public class RoleServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import test.task.sberData.repository.RoleRepository;
+import test.task.sberData.entities.Role;
+import test.task.sberData.service.RoleService;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class RoleServiceImpl implements RoleService {
+    private RoleRepository roleRepository;
+
+    @Autowired
+    public RoleServiceImpl(RoleRepository roleDao) {
+        this.roleRepository = roleDao;
+    }
+
+    @Override
+    public void addRole(Role role) {
+        roleRepository.save(role);
+    }
 }
