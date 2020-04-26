0406ef4569b4595682a8364116e36e8b39f18717
nick <gnn@mm.st>
2020-04-26 19:30:49 +0300

    add init in start

--- src/main/java/test/task/sberData/StartApp.java
+++ src/main/java/test/task/sberData/StartApp.java
@@ -1,4 +1,24 @@
 package test.task.sberData;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import test.task.sberData.initializer.DataInitializer;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
 public class StartApp {
+    public static void main(String[] args) {
+        SpringApplication.run(StartApp.class, args);
+    }
+
+
+    @Autowired
+    private DataInitializer dataInitializer;
+
+    @PostConstruct
+    public void initTestData() {
+        dataInitializer.init();
+    }
 }
