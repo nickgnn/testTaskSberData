1ed2feaa50f7edd4bc9c67cf2715700e8eabae1a
nick <gnn@mm.st>
2020-04-26 19:31:25 +0300

    add user services

--- src/main/java/test/task/sberData/service/UserListService.java
+++ src/main/java/test/task/sberData/service/UserListService.java
@@ -1,4 +1,13 @@
 package test.task.sberData.service;
 
-public interface UserService {
+import test.task.sberData.entities.User;
+
+import java.util.List;
+
+public interface UserListService {
+    List<User> getAllUsers();
+    User getOneUser(Long id);
+    User createUser(User user);
+    User updateUser(Long id, User user);
+    void deleteUser(Long id);
 }
--- src/main/java/test/task/sberData/service/impl/UserListServiceImpl.java
+++ src/main/java/test/task/sberData/service/impl/UserListServiceImpl.java
@@ -1,4 +1,37 @@
 package test.task.sberData.service.impl;
 
-public class UserListServiceImpl {
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import test.task.sberData.entities.User;
+import test.task.sberData.service.UserListService;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserListServiceImpl implements UserListService {
+    @Override
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public User getOneUser(Long id) {
+        return null;
+    }
+
+    @Override
+    public User createUser(User user) {
+        return null;
+    }
+
+    @Override
+    public User updateUser(Long id, User user) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+
+    }
 }
